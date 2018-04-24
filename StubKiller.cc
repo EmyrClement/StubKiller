e0458505f5bcab125f4079b6a88e97d8ee3e0192
Emyr John Clement <emyr.john.clement@cern.ch>
2018-04-24 14:12:13 +0100

    Modify to kill stubs in specified layers, and also kill fraction of stubs throughout tracker.

--- StubKiller.cc
+++ StubKiller.cc
@@ -7,29 +7,32 @@
 
 using namespace std;
 
-// layerToKill - which layer we are killing stubs in.
-// Barrel layers are encoded as 1-6. The endcap layers are encoded as 11-15 and 21-25
-// To kill stubs in all layers, set layerToKill == -1, however min/max phi/z will still be checked to decide whether the stub is killed or not
-// min/max Phi/Z - stubs within the region specified by these boundaries can be flagged for killing
-// fractionToKill - The fraction of stubs to kill.  An indentical stub in different events could be killed in one event but not the other
+// layersToKill - a vector stating the layers we are killing stubs in.  Can be an empty vector.
+// Barrel layers are encoded as 1-6. The endcap layers are encoded as 11-15 (-z) and 21-25 (+z)
+// min/max Phi/Z/R - stubs within the region specified by these boundaries and layersToKill are flagged for killing
+// fractionToKillInLayers - The fraction of stubs to kill in the specified layers/region.
+// fractionToKillEverywhere - The fraction of stubs to kill throughout the tracks
 
 bool killStub( const TTStub<Ref_Phase2TrackerDigi_>* stub,
 		const TrackerTopology* trackerTopology, 
 		const TrackerGeometry* trackerGeometry, 
-		const int layerToKill,
+		const vector<int> layersToKill,
 		const int minPhiToKill,
 		const int maxPhiToKill,
 		const int minZToKill,
 		const int maxZToKill,
-		const double fractionToKill = 1
+		const int minRToKill,
+		const int maxRToKill,
+		const double fractionToKillInLayers = 1,
+		const double fractionToKillEverywhere = 0
 	) {
 
-	DetId stackDetid = stub->getDetId();
-	DetId geoDetId(stackDetid.rawId() + 1);
-
-	// If we are killing stubs in all layers or a specific layer
-	if ( layerToKill > 0 ) {
+	// Only kill stubs in specified layers
+	if ( layersToKill.size() > 0 ) {
 		// Get the layer the stub is in, and check if it's in the layer you want to kill
+		DetId stackDetid = stub->getDetId();
+		DetId geoDetId(stackDetid.rawId() + 1);
+
 		bool isInBarrel = geoDetId.subdetId()==StripSubdetector::TOB || geoDetId.subdetId()==StripSubdetector::TIB;
 
 		int layerID = 0;
@@ -39,32 +42,37 @@ bool killStub( const TTStub<Ref_Phase2TrackerDigi_>* stub,
 		  layerID = 10*trackerTopology->side( geoDetId ) + trackerTopology->tidWheel( geoDetId );
 		}
 
-		if ( layerID != layerToKill ) return false;		
+		if ( find(layersToKill.begin(), layersToKill.end(), layerID ) != layersToKill.end() ) {
+			// Get the phi and z of stub, and check if it's in the region you want to kill
+			const GeomDetUnit* det0 = trackerGeometry->idToDetUnit( geoDetId );
+			const PixelGeomDetUnit* theGeomDet = dynamic_cast< const PixelGeomDetUnit* >( det0 );
+			const PixelTopology* topol = dynamic_cast< const PixelTopology* >( &(theGeomDet->specificTopology()) );
+			MeasurementPoint measurementPoint = stub->getClusterRef(0)->findAverageLocalCoordinatesCentered();
+			LocalPoint clustlp   = topol->localPosition(measurementPoint);
+			GlobalPoint pos  =  theGeomDet->surface().toGlobal(clustlp);
+
+			if ( pos.phi() > minPhiToKill && pos.phi() < maxPhiToKill &&
+				pos.z() > minZToKill && pos.z() < maxZToKill &&
+				pos.perp() > minRToKill && pos.perp() < maxRToKill ) {
+
+				// Kill fraction of stubs
+				static TRandom randomGenerator;
+				double randomNumber = randomGenerator.Rndm();
+				if ( randomGenerator.Rndm() < fractionToKillInLayers ) {
+					return true;
+				}
+			}
+		}
 	}
 
-
-	// Get the phi and z of stub, and check if it's in the region you want to kill
-	const GeomDetUnit* det0 = trackerGeometry->idToDetUnit( geoDetId );
-	const PixelGeomDetUnit* theGeomDet = dynamic_cast< const PixelGeomDetUnit* >( det0 );
-	const PixelTopology* topol = dynamic_cast< const PixelTopology* >( &(theGeomDet->specificTopology()) );
-	MeasurementPoint measurementPoint = stub->getClusterRef(0)->findAverageLocalCoordinatesCentered();
-	LocalPoint clustlp   = topol->localPosition(measurementPoint);
-	GlobalPoint pos  =  theGeomDet->surface().toGlobal(clustlp);
-
-	if ( pos.phi() > minPhiToKill && pos.phi() < maxPhiToKill &&
-		pos.z() > minZToKill && pos.z() < maxZToKill ) {
-
-		// Kill fraction of stubs
+	// Kill fraction of stubs throughout tracker
+	if ( fractionToKillEverywhere > 0 ) {
 		static TRandom randomGenerator;
 		double randomNumber = randomGenerator.Rndm();
-		if ( randomGenerator.Rndm() < fractionToKill ) {
+		if ( randomGenerator.Rndm() < fractionToKillEverywhere ) {
 			return true;
 		}
-		else {
-			return false;
-		}
 	}
-	else{
-		return false;	
-	}
+
+	return false;
 }
